c3b69a98459469bb874fddcf04a9c13f57b43401
Ryan <ryanmasonjar@gmail.com>
2024-11-27 22:10:16 -0500

    add wow you tried and clean up code

---
+++ gui/src/components/lastFiveGames.vue
@@ -0,0 +1,22 @@
+<script setup>
+const scores = defineModel('scores', { default: [] })
+</script>
+
+<template>
+  <div class="gap-2 ">
+    <h2 class="text-white">Last 5 Games</h2>
+    <ol class="grid gap-1 score-board text-center">
+      <li v-for="game in scores"><a class="arrow">
+          <i class="fas fa-arrow-alt-right"></i>
+          {{ game.player }} - {{ game.score }}
+        </a>
+      </li>
+    </ol>
+  </div>
+</template>
+<style>
+.score-board {
+  font-family: 'Press Start 2P';
+  font-size: 14px;
+}
+</style>
