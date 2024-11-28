93ed1da245e07fe612fe17bda6240778f07222d0
Ryan <ryanmasonjar@gmail.com>
2024-11-27 20:39:15 -0500

    add sound

---
+++ gui/src/views/UserView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useGameStore } from '@/stores/useGameStore';
+import { ref } from 'vue';
+const name = ref()
+const gameStore = useGameStore()
+function start() {
+  gameStore.startGame(name.value)
+}
+</script>
+<template>
+  <div class="about">
+    <h1 class="text-white text-h3">Who's playing</h1>
+    <input v-model="name" />
+
+    <button @click="start">Start</button>
+  </div>
+</template>
+
+<style></style>
